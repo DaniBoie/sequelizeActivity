27a252aa3b96d376229d8efb0c3ff34650ff0c14
Daniel A <dannyboy3385@gmail.com>
2020-08-27 14:08:13 -0700

    fixed tables

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const Syrup = require('./Syrup.js')
 const Topping = require('./Topping.js')
 const Drink = require('./Drink.js')
 
-Bean.belongsTo(Drink)
-Syrup.belongsTo(Drink)
-Topping.belongsTo(Drink)
-Drink.hasOne(Bean)
-Drink.hasOne(Topping)
-Drink.hasOne(Syrup)
+Bean.hasMany(Drink)
+Syrup.hasMany(Drink)
+Topping.hasMany(Drink)
+Drink.belongsTo(Bean)
+Drink.belongsTo(Topping)
+Drink.belongsTo(Syrup)
 
 module.exports = {Bean, Syrup, Topping, Drink}
