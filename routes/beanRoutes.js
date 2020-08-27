06c671bc176a08ecc2eb107649afe1a319cf2483
Adam Madueno <amadueno17@yahoo.com>
2020-08-27 14:08:18 -0700

    added to routes

--- routes/beanRoutes.js
+++ routes/beanRoutes.js
@@ -1,3 +1,11 @@
 const router = require('express').Router()
+router.get('/beans', (req, res) => {
+    Bean.findAll()
+      .then(beans => res.json(beans))
+      .catch(err => console.log(err))
+  })
+  
+
+  
 
 module.exports = router
